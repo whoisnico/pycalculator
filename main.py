d70dce08b602f0d892194bf20fc3b6bb091eccdb
nicotrixxel <104150729+nicotrixxel@users.noreply.github.com>
2023-02-22 13:58:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ root.configure(bg="darkgrey")
 
 
 write = tk.Entry(root)
-write.insert(0, "1")
+write.insert(0, "")
 write.pack()
 
 def button_klick(ziffer):
     write.insert(tk.END, ziffer)
 
-button1 = tk.Button(root, text="1", bg="lightgrey", fg="black", font=("Arial", 20), width=4, height=1, highlightbackground="white", highlightcolor="white", command=lambda: button_klick("1"))
+def result():
+    result = eval(write.get())
+    write.delete(0, tk.END)
+    write.insert(0, result)
+    
+
+button1 = tk.Button(root, text="1", bg="grey", font=("Arial", 20), width=4, height=1, borderwidth=0, command=lambda: button_klick("1"))
+button1.pack()
+
+button1 = tk.Button(root, text="=", bg="grey", font=("Arial", 20), width=4, height=1, borderwidth=10, command=result)
 button1.pack()
 
 root.mainloop()
